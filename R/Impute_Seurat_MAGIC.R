d8cc88b62c2d6c9fbe7eb41c0a20d99484f89fba
FloWuenne <flowuenne@gmail.com>
2018-07-05 15:03:02 -0400

    Edited MAGIC imputation function.

--- R/Impute_Seurat_MAGIC.R
+++ R/Impute_Seurat_MAGIC.R
@@ -1,6 +1,7 @@
-#' A Cat Function
+#' A function to perform imputation on a Seurat object using MAGIC.
 #'
-#' This function allows you to express your love of cats.
+#' This function enables you to easily impute the normalized gene expression of a Seurat object using the MAGIC package in R.
+#' More information about MAGIC can be found in the github repository.
 #' @param seurat_object The S4 Seurat object which contains filtered and normalized cells in the data slot.
 #' @keywords Seurat, MAGIC, imputation
 #' @export
@@ -11,10 +12,23 @@
 ## Imputation is performed on the filtered and normalized cells in the @data slot of the seurat object!
 ## The imputed expression matrix will then be saved in the @data
 
-## dependencies :
+## dependencies:
 ## Seurat : https://github.com/satijalab/seurat
 ## MAGIC : https://github.com/KrishnaswamyLab/MAGIC
 
-impute_seurat_MAGIC <- function(seurat_object){
+impute_seurat_MAGIC <- function(seurat_object,t_parameter='auto'){
 
+  ## transpose matrix for MAGIC
+  transposed_data <- t(seurat_object@data)
+
+  ## Actual imputation function
+  MAGIC_data <- magic(transposed_data,  genes="all_genes", t=t_parameter)
+
+  ## Retranspose the result for proper Seurat matrix format
+  imputed_data <- t(MAGIC_data$result)
+
+  ## Replace old Seurat data with imputed matrix
+  seurat_object@data <- imputed_data
+
+  return(seurat_object)
 }
