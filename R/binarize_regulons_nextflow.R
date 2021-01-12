0d9ea609fa72210a151f7270e61bdee008e8fc88
Florian Wuennemann <flowuenne@gmail.com>
2021-01-12 09:40:53 -0500

    Updated binarize function for nextflow implementation and updated tutorials.

--- R/binarize_regulons_nextflow.R
+++ R/binarize_regulons_nextflow.R
@@ -1,11 +1,12 @@
 #' This function is to analyze the nextflow implementation results of SCENIC!
 #' Uses AUC thresholds to binarize regulon activity. Binary regulons are stored as lists of dataframes.
 #'
-#' @param regulonAUC The AUC values for all regulons as calculated by SCENIC (content of file:3.4_regulonAUC.Rds).
-#' @param thresholds The AUC thresholds as determined by auc_thresh_kmeans or another method. Format has to be a named list with regulons as names and thresholds as values!
+#' @param regulonsAUC The AUC values for all regulons as calculated by SCENICprotocol.
+#' @param regulonsAucThresholds Output from get_regulonThresholds(loom) from SCENICoutput.
 #' @keywords SCENIC, regulons, binary activity, kmeans, thresholds
 #' @import SCENIC
 #' @import dplyr
+#' @import svMisc
 #' @export
 #' @examples
 #' \donttest{
@@ -14,29 +15,28 @@
 
 
 ## Binarize regulons based on thresholds
-binarize_regulons_nextflow <- function(regulonAUC,
-                              thresholds){
-  
+binarize_regulons_nextflow <- function(regulonsAUC,
+                                       regulonsAucThresholds){
+
   binary_regulon_list <- list()
-  binary_regulon_df <- data.frame("cells" = c())
-  
-  for(regulon_no in 1:length(names(thresholds))){
-    
+
+  for(regulon_no in 1:length(names(regulonsAucThresholds))){
+
     progress(regulon_no)
-    
-    regulon <- names(thresholds)[regulon_no]
-    
-    auc_df <-  data.frame("auc" = regulonAUC[,regulon],
-                          "cells"= rownames(regulonAUC))
-    
+
+    regulon <- names(regulonsAucThresholds)[regulon_no]
+
+    auc_df <-  data.frame("auc" = regulonsAUC[regulon,],
+                          "cells"= colnames(regulonsAUC))
+
     auc_df <- auc_df %>%
-      mutate("regulon"= if_else(auc >= thresholds[regulon],1,0)) %>%
+      mutate("regulon"= if_else(auc >= regulonsAucThresholds[regulon],1,0)) %>%
       select(-auc)
-    
+
     colnames(auc_df) <- c("cells",regulon)
-    
+
     binary_regulon_list[[regulon]] <- auc_df
   }
-  
+
   return(binary_regulon_list)
 }
